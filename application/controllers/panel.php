c62b1c71429ea82c8997cd47291083d2a74b76f7
Jose Carlos <karlosgg@gmail.com>
2017-06-14 23:57:54 -0600

    panelA

---
+++ application/controllers/panel.php
@@ -0,0 +1,17 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Panel extends CI_Controller {
+
+	public function index()
+	{
+		//$usuario=$data['Usuario'];
+		//$usuario="Jose";
+		//if($usuario!=null){
+			$this->load->view('panel');
+		//}
+		//else{
+		//	$this->load->view('login');
+		//}
+	}
+}
+
--- application/views/panel.php
+++ application/views/panel.php
@@ -1,6 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-	require_once APPPATH.'views/panel/header.html';
-	require_once APPPATH.'views/panel/login.html';
-	require_once APPPATH.'views/panel/footer.html';
+	require_once APPPATH.'views/panelA/header.html';
+	require_once APPPATH.'views/panelA/login.html';
+	require_once APPPATH.'views/panelA/footer.html';
